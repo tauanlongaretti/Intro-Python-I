b023165c457b5d11eb5f2ee64b918e19999bd16d
tauan-longaretti <tauanlongaretti@gmail.com>
2020-09-04 22:35:23 -0400

    file set of problems completed

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -13,6 +13,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+with open('c:/Users/imgui/Documents/GitFolder/Computer-Science/Intro-Python-I/src/foo.txt') as f:
+    read_data = f.read()
+print(read_data)
+print(f.closed)
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -20,3 +24,10 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+f = open('bar.txt', 'w')
+f.write("I am learning Python.\nI am really enjoying it.\nHopefully it doesn't get harder")
+
+f.close()
+
+f = open('bar.txt', 'r')
+print(f.read())
